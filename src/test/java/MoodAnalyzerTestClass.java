31ed1537fc0e8bb605fe5bc24a4c57bf5ffbe7e8
rajchaudhari09 <rajchaudhari366@gmail.com>
2020-04-18 08:52:48 +0530

    ADDED: Given happy message when improper method should throws mood analysis exception

--- src/test/java/MoodAnalyzerTestClass.java
+++ src/test/java/MoodAnalyzerTestClass.java
@@ -1,6 +1,5 @@
 import org.junit.Assert;
 import org.junit.Test;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
@@ -70,7 +69,7 @@ public class MoodAnalyzerTestClass {
     public void givenConstructorName_WhenImproper_ThenThrowMoodAnalysisException() {
         try {
             MoodAnalyzer mood = new MoodAnalyzer();
-            MoodAnalyserFactory.getConstructor("MoodAnalyzer", Integer.class);
+            MoodAnalyserFactory.getConstructor("MoodAnalyzer",Integer.class);
         } catch (MoodAnalysisException e) {
             Assert.assertEquals(MoodAnalysisException.MyException_Type.METHOD_NOT_FOUND, e.type);
         }
@@ -114,12 +113,26 @@ public class MoodAnalyzerTestClass {
     @Test
     public void givenHappy_WhenProper_ThenReturnHappyMood() throws MoodAnalysisException {
         try {
-            Constructor<?> constructor = MoodAnalyserFactory.getConstructor("MoodAnalyzer",String.class);
+            Constructor<?> constructor = MoodAnalyserFactory.getConstructor("MoodAnalyzer", String.class);
             Object instance = constructor.newInstance("i am in happy mood");
             String analyser = MoodAnalyserFactory.moodAnalyzer ( (MoodAnalyzer) instance ,"moodAnalyzer") ;
             Assert.assertEquals("happy",analyser);
         } catch (IllegalAccessException |InstantiationException | InvocationTargetException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void givenHappyMessage_WhenImproperMethod_ShouldThrowMoodAnalysisException() {
+        try {
+            Constructor<?> constructor = MoodAnalyserFactory.getConstructor("MoodAnalyzer", Integer.class);
+            Object instance = constructor.newInstance("i am in happy mood");
+            String analyser = MoodAnalyserFactory.moodAnalyzer ( (MoodAnalyzer) instance ,"moodAnalyzer") ;
+            Assert.assertEquals("happy",analyser);
+        }catch (MoodAnalysisException e) {
+            Assert.assertEquals(MoodAnalysisException.MyException_Type.METHOD_NOT_FOUND,e.type);
+        } catch (IllegalAccessException |InstantiationException |InvocationTargetException  e ) {
+            e.printStackTrace();
+        }catch (RuntimeException e){e.printStackTrace();}
     }
 }
